104130a1adf930b36e4ac35ae077507b9b79fd5f
fadeev-aleksandr <1alexking1@gmail.com>
2017-05-24 22:52:57 +0300

    Leg Movement: Added ground level

--- scripts/legmovement.js
+++ scripts/legmovement.js
@@ -1,3 +1,18 @@
+function drawLine(context, x1, y1, x2, y2, c) {
+    context.strokeStyle = c;
+    context.beginPath();
+    context.moveTo(x1, y1);
+    context.lineTo(x2, y2);
+    context.stroke();
+}
+
+function drawCircle(context, x, y, r, c) {
+    context.strokeStyle = c;
+    context.beginPath();
+    context.arc(x, y, r, 0, 2 * Math.PI);
+    context.stroke();
+}
+
 class Leg {
 
     constructor(canvas, c1, c2, c3) {
@@ -8,22 +23,32 @@ class Leg {
         this.c3 = c3;
     }
 
-    drawLine(x1, y1, x2, y2, c) {
-        this.context.strokeStyle = c;
-        this.context.beginPath();
-        this.context.moveTo(x1, y1);
-        this.context.lineTo(x2, y2);
-        this.context.stroke();
+    setState(aAngle, bAngle) {
+        let h = this.canvas.height;
+        let w = this.canvas.width;
+        let l = Math.abs(this.l1) + Math.abs(this.l2);
+        let m = Math.min(w / 2, h);
+        let l1 = m * 8 / 10 * this.l1 / l;
+        let l2 = m * 8 / 10 * this.l2 / l;
+
+        this.x1 = w / 2;
+        this.y1 = h / 10;
+        this.x2 = this.x1 + l1 * Math.cos(aAngle);
+        this.y2 = this.y1 + l1 * Math.sin(aAngle);
+        this.x3 = this.x2 + l2 * Math.cos(bAngle);
+        this.y3 = this.y2 + l2 * Math.sin(bAngle);
+        this.x4 = this.x3 + l2 * Math.cos(bAngle + Math.PI / 2) / 4;
+        this.y4 = this.y3 + l2 * Math.sin(bAngle + Math.PI / 2) / 4;
+        let r = h / 50;
     }
 
-    drawCircle(x, y, r, c) {
-        this.context.strokeStyle = c;
-        this.context.beginPath();
-        this.context.arc(x, y, r, 0, 2 * Math.PI);
-        this.context.stroke();
+    get height() {
+        let yMin = Math.min(this.y1, this.y2, this.y3, this.y4);
+        let yMax = Math.max(this.y1, this.y2, this.y3, this.y4);
+        return (yMax - yMin)
     }
 
-    draw(aAngle, bAngle) {
+    draw(aAngle, bAngle, height) {
         let h = this.canvas.height;
         let w = this.canvas.width;
 
@@ -40,17 +65,23 @@ class Leg {
         let y3 = y2 + l2 * Math.sin(bAngle);
         let x4 = x3 + l2 * Math.cos(bAngle + Math.PI / 2) / 4;
         let y4 = y3 + l2 * Math.sin(bAngle + Math.PI / 2) / 4;
+        let dy = height - (9 / 10 * h - Math.min(y1, y2, y3, y4));
+        y1 -= dy;
+        y2 -= dy;
+        y3 -= dy;
+        y4 -= dy;
+
         let r = h / 50;
 
         this.context.lineWidth = 3;
 
-        this.drawLine(x1, y1, x2, y2, this.c1);
-        this.drawLine(x2, y2, x3, y3, this.c2);
-        this.drawLine(x3, y3, x4, y4, this.c2);
+        drawLine(this.context, x1, y1, x2, y2, this.c1);
+        drawLine(this.context, x2, y2, x3, y3, this.c2);
+        drawLine(this.context, x3, y3, x4, y4, this.c2);
 
-        this.drawCircle(x1, y1, r, this.c3);
-        this.drawCircle(x2, y2, r, this.c3);
-        this.drawCircle(x3, y3, r, this.c3);
+        drawCircle(this.context, x1, y1, r, this.c3);
+        drawCircle(this.context, x2, y2, r, this.c3);
+        drawCircle(this.context, x3, y3, r, this.c3);
     }
 }
 
@@ -206,9 +237,11 @@ class Drawer {
         let h = this.canvas.height;
         let w = this.canvas.width;
         this.context.clearRect(0, 0, w, h);
+        drawLine(this.context, 0, 9 / 10 * h, w, 9 / 10 * h);
 
-        this.legBack.draw(this.aAngle(t2), this.bAngle(t2));
-        this.legFront.draw(this.aAngle(t1), this.bAngle(t1));
+        let height = Math.max(this.legFront.height, this.legBack.height);
+        this.legBack.draw(this.aAngle(t2), this.bAngle(t2), height);
+        this.legFront.draw(this.aAngle(t1), this.bAngle(t1), height);
     }
 
     tick() {
@@ -217,17 +250,16 @@ class Drawer {
         if (t2 < this.t0) {
             t2 += this.tf - this.t0;
         }
+        this.legFront.setState(this.aAngle(t1), this.bAngle(t1));
+        this.legBack.setState(this.aAngle(t2), this.bAngle(t2));
+
         this.draw(t1, t2);
-        this.t += 1 / 600;
-        if (this.t < this.tf) {
-            window.requestAnimationFrame(() => { this.tick() });
-        } else {
-            if (this.t > this.tf) {
-                this.t += this.t0 - this.tf;
-            }
-            window.requestAnimationFrame(() => { this.tick() });
-            //setTimeout(() => { window.requestAnimationFrame(() => { this.tick() }) }, 1000);
+
+        this.t += 1 / 60;
+        if (this.t > this.tf) {
+            this.t += this.t0 - this.tf;
         }
+        window.requestAnimationFrame(() => { this.tick() });
     }
 
     start() {
