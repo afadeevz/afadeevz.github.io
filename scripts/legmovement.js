50feaefa1d1f2fc880d8252e3f75924d13ff67ed
fadeev-aleksandr <1alexking1@gmail.com>
2017-05-24 16:07:51 +0300

    Leg Movement: Added second leg

--- scripts/legmovement.js
+++ scripts/legmovement.js
@@ -1,3 +1,59 @@
+class Leg {
+
+    constructor(canvas, c1, c2, c3) {
+        this.canvas = canvas;
+        this.context = canvas.getContext("2d");
+        this.c1 = c1;
+        this.c2 = c2;
+        this.c3 = c3;
+    }
+
+    drawLine(x1, y1, x2, y2, c) {
+        this.context.strokeStyle = c;
+        this.context.beginPath();
+        this.context.moveTo(x1, y1);
+        this.context.lineTo(x2, y2);
+        this.context.stroke();
+    }
+
+    drawCircle(x, y, r, c) {
+        this.context.strokeStyle = c;
+        this.context.beginPath();
+        this.context.arc(x, y, r, 0, 2 * Math.PI);
+        this.context.stroke();
+    }
+
+    draw(aAngle, bAngle) {
+        let h = this.canvas.height;
+        let w = this.canvas.width;
+
+        let l = Math.abs(this.l1) + Math.abs(this.l2);
+        let m = Math.min(w / 2, h);
+        let l1 = m * 8 / 10 * this.l1 / l;
+        let l2 = m * 8 / 10 * this.l2 / l;
+
+        let x1 = w / 2;
+        let y1 = h / 10;
+        let x2 = x1 + l1 * Math.cos(aAngle);
+        let y2 = y1 + l1 * Math.sin(aAngle);
+        let x3 = x2 + l2 * Math.cos(bAngle);
+        let y3 = y2 + l2 * Math.sin(bAngle);
+        let x4 = x3 + l2 * Math.cos(bAngle + Math.PI / 2) / 4;
+        let y4 = y3 + l2 * Math.sin(bAngle + Math.PI / 2) / 4;
+        let r = h / 50;
+
+        this.context.lineWidth = 3;
+
+        this.drawLine(x1, y1, x2, y2, this.c1);
+        this.drawLine(x2, y2, x3, y3, this.c2);
+        this.drawLine(x3, y3, x4, y4, this.c2);
+
+        this.drawCircle(x1, y1, r, this.c3);
+        this.drawCircle(x2, y2, r, this.c3);
+        this.drawCircle(x3, y3, r, this.c3);
+    }
+}
+
 class Drawer {
 
     aAngle(t) {
@@ -24,11 +80,11 @@ class Drawer {
 
     formula2expression(formula, defaultValue = "0") {
         var expression = formula;
-        expression = expression.replace(/ /g, "")
+        expression = expression.replace(/ /g, "");
         expression = expression.replace(/Math\.sin/gi, "sin").replace(/sin/gi, "Math.sin");
         expression = expression.replace(/Math\.cos/gi, "cos").replace(/cos/gi, "Math.cos");
         expression = expression.replace(/Math\.PI/gi, "PI").replace(/PI/gi, "Math.PI");
-        expression = expression.replace(/\^/gi, "**")
+        expression = expression.replace(/\^/gi, "**");
         if (expression)
             return expression;
         else
@@ -43,6 +99,8 @@ class Drawer {
             this.l1 = 1;
             lUp.style.backgroundColor = "red";
         }
+        this.legFront.l1 = this.l1;
+        this.legBack.l1 = this.l1;
         console.log("l1 = " + this.l1);
         try {
             this.l2 = (new Function('return (' + this.formula2expression(lDown.value) + ');'))();
@@ -51,6 +109,8 @@ class Drawer {
             this.l2 = 1;
             lDown.style.backgroundColor = "red";
         }
+        this.legFront.l2 = this.l2;
+        this.legBack.l2 = this.l2;
         console.log("l2 = " + this.l2);
         try {
             this.a0 = (new Function('return (' + this.formula2expression(aStart.value) + ');'))();
@@ -84,6 +144,14 @@ class Drawer {
             tFinish.style.backgroundColor = "red";
         }
         console.log("tf = " + this.tf);
+        try {
+            this.dt = (new Function('return (' + this.formula2expression(dt.value, 0) + ');'))();
+            dt.style.backgroundColor = "";
+        } catch (error) {
+            this.dt = 0;
+            dt.style.backgroundColor = "red";
+        }
+        console.log("dt = " + this.dt);
         try {
             this.a = new Function('t', 'return (' + this.formula2expression(at.value) + ');');
             this.a(this.t0);
@@ -124,6 +192,8 @@ class Drawer {
     constructor(canvas) {
         this.canvas = canvas;
         this.context = this.canvas.getContext("2d");
+        this.legFront = new Leg(canvas, "#EE0000", "#00EE00", "#000000");
+        this.legBack = new Leg(canvas, "#EE7777", "#77EE77", "#777777");
         this.update();
         this.t = this.t0;
         window.addEventListener("input", (e) => { this.update(e) });
@@ -132,71 +202,31 @@ class Drawer {
         window.addEventListener("resize", (e) => { this.resize(e) });
     }
 
-    draw() {
+    draw(t1, t2) {
         let h = this.canvas.height;
         let w = this.canvas.width;
         this.context.clearRect(0, 0, w, h);
 
-        let l = Math.abs(this.l1) + Math.abs(this.l2);
-        let m = Math.min(w, h);
-        let l1 = m * 8 / 10 * this.l1 / l;
-        let l2 = m * 8 / 10 * this.l2 / l;
-
-        let x = w / 2;
-        let y = h / 10;
-        let r = h / 50;
-        this.context.lineWidth = 3;
-
-        this.context.strokeStyle = "#EE0000";
-        this.context.beginPath();
-        this.context.moveTo(x, y);
-        x += l1 * Math.cos(this.aAngle(this.t));
-        y += l1 * Math.sin(this.aAngle(this.t));
-        this.context.lineTo(x, y);
-        this.context.stroke();
-
-        this.context.strokeStyle = "#00EE00";
-        this.context.beginPath();
-        this.context.moveTo(x, y);
-        x += l2 * Math.cos(this.bAngle(this.t));
-        y += l2 * Math.sin(this.bAngle(this.t));
-        this.context.lineTo(x, y);
-        this.context.stroke();
-
-        x = w / 2;
-        y = h / 10;
-        r = h / 50;
-
-        this.context.strokeStyle = "#000000";
-        this.context.beginPath();
-        this.context.arc(x, y, r, 0, 2 * Math.PI);
-        this.context.stroke();
-
-        x += l1 * Math.cos(this.aAngle(this.t));
-        y += l1 * Math.sin(this.aAngle(this.t));
-
-        this.context.strokeStyle = "#000000";
-        this.context.beginPath();
-        this.context.arc(x, y, r, 0, 2 * Math.PI);
-        this.context.stroke();
-
-        x += l2 * Math.cos(this.bAngle(this.t));
-        y += l2 * Math.sin(this.bAngle(this.t));
-
-        this.context.strokeStyle = "#000000";
-        this.context.beginPath();
-        this.context.arc(x, y, r, 0, 2 * Math.PI);
-        this.context.stroke();
+        this.legBack.draw(this.aAngle(t2), this.bAngle(t2));
+        this.legFront.draw(this.aAngle(t1), this.bAngle(t1));
     }
 
     tick() {
-        this.draw();
-        this.t += 1 / 60;
-        if (this.t < this.tf)
+        let t1 = this.t;
+        let t2 = this.t - this.dt;
+        if (t2 < this.t0) {
+            t2 += this.tf - this.t0;
+        }
+        this.draw(t1, t2);
+        this.t += 1 / 600;
+        if (this.t < this.tf) {
             window.requestAnimationFrame(() => { this.tick() });
-        else {
-            this.t = this.t0;
-            setTimeout(() => { window.requestAnimationFrame(() => { this.tick() }) }, 1000);
+        } else {
+            if (this.t > this.tf) {
+                this.t += this.t0 - this.tf;
+            }
+            window.requestAnimationFrame(() => { this.tick() });
+            //setTimeout(() => { window.requestAnimationFrame(() => { this.tick() }) }, 1000);
         }
     }
 
