0cd5b7f8f94696b7a3ead191872fb2f81097b644
Alexander Fadeev <zanderfadeev@gmail.com>
2024-04-09 02:56:23 +0300

    Corsa

---
+++ project/corsa/index.js
@@ -0,0 +1,33 @@
+
+const startTimeMS = Date.parse("2024-04-10T12:00:00.000+03:00")
+const endTimeMS = Date.parse("2024-05-10T12:00:00.000+03:00")
+const startCost = 450000;
+const endCost = 350000;
+
+function lerp(progress, start, end) {
+    return start * (1 - progress) + end * progress;
+}
+
+function tick() {
+    let now = new Date();
+
+    let nowMS = now.getTime();
+
+    let totalTime = endTimeMS - startTimeMS;
+    let elapsedTime = nowMS - startTimeMS
+
+    progress = elapsedTime / totalTime;
+
+    let currentCost = lerp(progress, startCost, endCost);
+
+    let costElement = document.getElementById("cost");
+    costElement.innerHTML = currentCost.toFixed(2) + "₽"
+
+    requestAnimationFrame(tick)
+}
+
+function main() {
+    tick()
+}
+
+main()
