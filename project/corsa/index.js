cc34f9bc60e041a980b5953883eaad1adb47c46e
Alexander Fadeev <zanderfadeev@gmail.com>
2024-08-19 16:46:09 +0300

    [Corsa] Update lerp values

--- project/corsa/index.js
+++ project/corsa/index.js
@@ -1,7 +1,7 @@
 
-const startTimeMS = Date.parse("2024-06-26T12:00:00.000+03:00")
-const endTimeMS = Date.parse("2024-10-01T12:00:00.000+03:00")
-const startCost = 500000;
+const startTimeMS = Date.parse("2024-08-19T12:00:00.000+03:00")
+const endTimeMS = Date.parse("2025-01-01T12:00:00.000+03:00")
+const startCost = 389000;
 const endCost = 300000;
 
 function lerp(progress, start, end) {
