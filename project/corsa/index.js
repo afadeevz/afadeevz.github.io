ad09c3481a9e6507a66d37bf6f95bd1f582f52a1
afadeevz <zanderfadeev@gmail.com>
2024-12-07 15:16:55 +0300

    Update index.js

--- project/corsa/index.js
+++ project/corsa/index.js
@@ -1,7 +1,7 @@
 
-const startTimeMS = Date.parse("2024-08-19T12:00:00.000+03:00")
-const endTimeMS = Date.parse("2025-01-01T12:00:00.000+03:00")
-const startCost = 389000;
+const startTimeMS = Date.parse("2024-12-07T12:00:00.000+03:00")
+const endTimeMS = Date.parse("2025-02-28T12:00:00.000+03:00")
+const startCost = 340000;
 const endCost = 300000;
 
 function lerp(progress, start, end) {
