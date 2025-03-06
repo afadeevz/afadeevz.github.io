5abeb66a5a9423a859fe57078c64d4c61461768a
Alexander Fadeev <zanderfadeev@gmail.com>
2025-03-06 15:50:58 +0300

    [Corsa] Update

--- project/corsa/index.js
+++ project/corsa/index.js
@@ -1,24 +1,18 @@
 
-const startTimeMS = Date.parse("2024-08-19T12:00:00.000+03:00")
-const endTimeMS = Date.parse("2025-01-01T12:00:00.000+03:00")
-const startCost = 389000;
-const endCost = 300000;
-
-function lerp(progress, start, end) {
-    return start * (1 - progress) + end * progress;
-}
+const startTimeMS = Date.parse("2025-03-06T16:00:00.000+03:00")
+const baseCost = 330000;
+const periodMS = 30 * 24 * 60 * 60 * 1000; // 1 Month
+const discountPerPeriod = 0.03 // 3%
 
 function tick() {
     let now = new Date();
 
     let nowMS = now.getTime();
 
-    let totalTime = endTimeMS - startTimeMS;
     let elapsedTime = nowMS - startTimeMS
+    let elapsedPeriods = elapsedTime / periodMS;
 
-    progress = elapsedTime / totalTime;
-
-    let currentCost = lerp(progress, startCost, endCost);
+    let currentCost = baseCost * Math.pow(1 - discountPerPeriod, elapsedPeriods);
 
     let costElement = document.getElementById("cost");
     costElement.innerHTML = currentCost.toFixed(2) + "₽"
